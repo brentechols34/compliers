133e072d5c11596109835a41a5e46391edf65150
Kalvyn-Lu <kalvynlu@gmail.com>
2015-01-26 21:18:59 -0700

    Enum

--- scan/State.java
+++ scan/State.java
@@ -1,5 +1,21 @@
 package scan;
 
+import java.util.Scanner;
+
 public enum State {
+	LETTER (""),
+	DIGIT (""),
+	LEFT_PAREN (""),
+	RIGHT_PAREN (""),
+	OPERATOR ("");
 	
+	private final String regex;
+			
+	private State(String regex){
+		this.regex = regex;
+	}
+	
+	public String run(Scanner s){
+		return "";
+	}
 }
