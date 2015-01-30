1b403de4b6b64426f11e8900627f7fb7a66c64f2
Alex-Huleatt <ahuleatt@gmail.com>
2015-01-29 20:40:18 -0700

    Stupid way

--- scan2/FSA.java
+++ scan2/FSA.java
@@ -1,6 +1,8 @@
 package scan2;
 
+import java.io.PushbackReader;
+
 public interface FSA {
-	
+	public Token run(PushbackReader sc);
 
 }
---
+++ scan2/Token.java
@@ -0,0 +1,5 @@
+package scan2;
+
+public class Token {
+
+}
