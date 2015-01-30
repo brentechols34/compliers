bfb484da5623dddf41130487af4f498bce2bf16c
Alex-Huleatt <ahuleatt@gmail.com>
2015-01-29 20:43:35 -0700

    Stupid way

---
+++ scan2/Identifier_FSA.java
@@ -0,0 +1,13 @@
+package scan2;
+
+import java.io.PushbackReader;
+
+public class Identifier_FSA implements FSA {
+
+	@Override
+	public Token run(PushbackReader sc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ scan2/SingleString_FSA.java
@@ -0,0 +1,5 @@
+package scan2;
+
+public class SingleString_FSA {
+
+}
