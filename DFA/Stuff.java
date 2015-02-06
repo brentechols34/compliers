25bf1fac8d6604f6dba0e890efd8f2cc09e71a0f
Alex-Huleatt <ahuleatt@gmail.com>
2015-02-06 15:47:32 -0700

    Commiting

--- DFA/Stuff.java
+++ DFA/Stuff.java
@@ -10,14 +10,20 @@ public class Stuff {
 	private static String unclosedComment = "a:({)b b<<:(\\a\\s)b";
 	private static String comment = "a:({)b b:(\\a\\s\\d)b,(})c c<<:()";
 	private static String stringLiteral = "a:(')b b:(\\a\\d\\s)b,(')c c<<:(')b";
-	private static String plus = "a:(+)b b<<:()b"; //+
-	private static String equality = "a:(<>)b,(=)c b<<:(=)c c<<:()"; //=<, <, >=, >, =
+	private static String arith = "a:(+-*/)b b<<:()b"; //+
+	private static String equality = "a:(<>)b b<<:(=)c c<<:()"; //<=, <, >=, >, =
+	private static String nequal = "a:(<)b b:(>)c c<<:()c"; // <>
+	private static String oparen = "a:(\\()b b<<:()b";
+	private static String cparen = "a:(\\))b b<<:()b";
 	private static String set = "a:(:)b b:(=)c c<<:()c"; //:=
+	private static String scolon = "a:(;)b b<<:()b";
 	
 	private static String[] reserved = new String[] {"and", "begin", "Boolean", "div", "do", "downto", "else", "end", "false", "fixed", "float", "for", "function", "if", "integer", "mod", "not", "or", "procedure", "program", "read", "repeat", "string", "then", "true", "to", "type", "until", "var", "while", "write", "writeln"};
 	public static void main(String[] args) throws FileNotFoundException {
-		String[] stuff = new String[] {integer,fixed,fp,identifier, unclosedComment, comment, stringLiteral,plus,equality};
-		String[] names = new String[] {"integer","fixed","fp","identifier", "unclosedComment", "comment", "stringLiteral", "plus", "equality"};
+		String[] stuff = new String[] {integer,fixed,fp,identifier, unclosedComment, 
+				comment, stringLiteral, arith, equality, nequal, oparen, cparen, set, scolon};
+		String[] names = new String[] {"integer","fixed","fp","identifier", "unclosedComment", 
+				"comment", "stringLiteral", "arith", "equality", "nequal", "oparen", "cparen", "set", "scolon"};
 		Generator.generate(names, stuff, reserved);
 	}
 }
