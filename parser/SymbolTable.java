521d52b284b453e81709f3471de03e87d6f689cc
Kalvyn-Lu <kalvynlu@gmail.com>
2015-02-27 17:04:55 -0700

    Symbol table done

--- parser/SymbolTable.java
+++ parser/SymbolTable.java
@@ -1,9 +1,43 @@
 package parser;
 
+import java.util.Hashtable;
+
 public class SymbolTable {
 	
 	String name, label;
 	int nestingLevel;
 	
+	Hashtable<String,TableEntry> entries;
 	
+	public SymbolTable(String name, String label, int nestingLevel){
+		this.name = name;
+		this.label = label;
+		this.nestingLevel = nestingLevel;
+		
+		entries = new Hashtable<String,TableEntry>();
+	}
+	
+	//Encompasses All
+	public void addEntry(String lexeme, String type, String kind, String mode, int size, String[] parameters){
+		TableEntry entry = new TableEntry(type,kind,mode,size,parameters);
+		entries.put(lexeme, entry);
+	}
+	
+	//Variables
+	public void addEntry(String lexeme, String type, String kind, int size){
+		TableEntry entry = new TableEntry(type,kind,size);
+		entries.put(lexeme, entry);
+	}
+
+	//Parameters
+	public void addEntry(String lexeme, String type, String kind, String mode, int size){
+		TableEntry entry = new TableEntry(type,kind,mode,size);
+		entries.put(lexeme, entry);
+	}
+	
+	//Procedures/functions
+	public void addEntry(String lexeme, String kind, String[] parameters){
+		TableEntry entry = new TableEntry(kind,parameters);
+		entries.put(lexeme, entry);
+	}
 }
--- parser/TableEntry.java
+++ parser/TableEntry.java
@@ -1,7 +1,42 @@
 package parser;
 
-import java.util.Hashtable;
-
 public class TableEntry {
-
+	
+	String type,
+		kind,
+		mode;
+	
+	int size;
+	
+	String[] parameters;
+	
+	//Encompasses All
+	public TableEntry(String type, String kind, String mode, int size, String[] parameters){
+		this.type = type;
+		this.kind = kind;
+		this.mode = mode;
+		this.size = size;
+		this.parameters = parameters;
+	}
+	
+	//Variables
+	public TableEntry(String type, String kind, int size){
+		this.type = type;
+		this.kind = kind;
+		this.size = size;
+	}
+	
+	//Parameters
+	public TableEntry(String type, String kind, String mode, int size){
+		this.type = type;
+		this.kind = kind;
+		this.mode = mode;
+		this.size = size;
+	}
+	
+	//Procedures/functions
+	public TableEntry(String kind, String[] parameters){
+		this.parameters = parameters;
+		this.kind = kind;
+	}
 }
