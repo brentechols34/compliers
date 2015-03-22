c5cd86759aaf26ce2afbe14ed6c9bcfd4653cfdb
Brent Echols <brent.echols@webfilings.com>
2015-03-22 01:46:15 -0600

    fixed LL to be right

--- LLTableGenerator.js
+++ LLTableGenerator.js
@@ -1,5 +1,7 @@
 var arr = 'SystemGoal: Program MP_EOF\nProgram: ProgramHeading MP_SCOLON Block MP_PERIOD\nProgramHeading: MP_PROGRAM ProgramIdentifier\nBlock: VariableDeclarationPart ProcedureAndFunctionDeclarationPart StatementPart\nVariableDeclarationPart: MP_VAR VariableDeclaration MP_SCOLON VariableDeclarationTail\nVariableDeclarationPart: lambda\nVariableDeclarationTail: VariableDeclaration MP_SCOLON VariableDeclarationTail\nVariableDeclarationTail: lambda\nVariableDeclaration: IdentifierList MP_COLON Type\nType: MP_INTEGER\nType: MP_FLOAT\nType: MP_STRING\nType: MP_BOOLEAN\nProcedureAndFunctionDeclarationPart: ProcedureDeclaration ProcedureAndFunctionDeclarationPart\nProcedureAndFunctionDeclarationPart: FunctionDeclaration ProcedureAndFunctionDeclarationPart\nProcedureAndFunctionDeclarationPart: lambda\nProcedureDeclaration: ProcedureHeading MP_SCOLON Block MP_SCOLON\nFunctionDeclaration: FunctionHeading MP_SCOLON Block MP_SCOLON\nProcedureHeading: MP_PROCEDURE ProcedureIdentifier OptionalFormalParameterList\nFunctionHeading: MP_FUNCTION FunctionIdentifier OptionalFormalParameterList MP_COLON Type\nOptionalFormalParameterList: MP_LPAREN FormalParameterSection FormalParameterSectionTail MP_RPAREN\nOptionalFormalParameterList: lambda\nFormalParameterSectionTail: MP_SCOLON FormalParameterSection FormalParameterSectionTail\nFormalParameterSectionTail: lambda\nFormalParameterSection: ValueParameterSection\nFormalParameterSection: VariableParameterSection\nValueParameterSection: IdentifierList MP_COLON Type\nVariableParameterSection: MP_VAR IdentifierList MP_COLON Type\nStatementPart: CompoundStatement\nCompoundStatement: MP_BEGIN StatementSequence MP_END\nStatementSequence: Statement StatementTail\nStatementTail: MP_SCOLON Statement StatementTail\nStatementTail: lambda\nStatement: EmptyStatement\nStatement: CompoundStatement\nStatement: ReadStatement\nStatement: WriteStatement\nStatement: AssignmentStatement\nStatement: IfStatement\nStatement: WhileStatement\nStatement: RepeatStatement\nStatement: ForStatement\nStatement: ProcedureStatement\nEmptyStatement: lambda\nReadStatement: MP_READ MP_LPAREN ReadParameter ReadParameterTail MP_RPAREN\nReadParameterTail: MP_COMMA ReadParameter ReadParameterTail\nReadParameterTail: lambda\nReadParameter: VariableIdentifier\nWriteStatement: MP_WRITE MP_LPAREN WriteParameter WriteParameterTail MP_RPAREN\nWriteStatement: MP_WRITELN MP_LPAREN WriteParameter WriteParameterTail MP_RPAREN\nWriteParameterTail: MP_COMMA WriteParameter WriteParameterTail\nWriteParameterTail: lambda\nWriteParameter: OrdinalExpression\nAssignmentStatement: VariableIdentifier MP_ASSIGN Expression\nAssignmentStatement: FunctionIdentifier MP_ASSIGN Expression\nIfStatement: MP_IF BooleanExpression MP_THEN Statement OptionalElsePart\nOptionalElsePart: MP_ELSE Statement\nOptionalElsePart: lambda\nRepeatStatement: MP_REPEAT StatementSequence MP_UNTIL BooleanExpression\nWhileStatement: MP_WHILE BooleanExpression MP_DO Statement\nForStatement: MP_FOR ControlVariable MP_ASSIGN InitialValue StepValue FinalValue MP_DO Statement\nControlVariable: VariableIdentifier\nInitialValue: OrdinalExpression\nStepValue: MP_TO\nStepValue: MP_DOWNTO\nFinalValue: OrdinalExpression\nProcedureStatement: ProcedureIdentifier OptionalActualParameterList\nOptionalActualParameterList: MP_LPAREN ActualParameter ActualParameterTail MP_RPAREN\nOptionalActualParameterList: lambda\nActualParameterTail: MP_COMMA ActualParameter ActualParameterTail\nActualParameterTail: lambda\nActualParameter: OrdinalExpression\nExpression: SimpleExpression OptionalRelationalPart\nOptionalRelationalPart: RelationalOperator SimpleExpression\nOptionalRelationalPart: lambda\nRelationalOperator: MP_EQUAL\nRelationalOperator: MP_LTHAN\nRelationalOperator: MP_GTHAN\nRelationalOperator: MP_LEQUAL\nRelationalOperator: MP_GEQUAL\nRelationalOperator: MP_NEQUAL\nSimpleExpression: OptionalSign Term TermTail\nTermTail: AddingOperator Term TermTail\nTermTail: lambda\nOptionalSign: MP_PLUS\nOptionalSign: MP_MINUS\nOptionalSign: lambda\nAddingOperator: MP_PLUS\nAddingOperator: MP_MINUS\nAddingOperator: MP_OR\nTerm: Factor FactorTail\nFactorTail: MultiplyingOperator Factor FactorTail\nFactorTail: lambda\nMultiplyingOperator: MP_TIMES\nMultiplyingOperator: MP_FLOAT_DIVIDE\nMultiplyingOperator: MP_DIV\nMultiplyingOperator: MP_MOD\nMultiplyingOperator: MP_AND\nFactor: MP_INTEGER_LIT\nFactor: MP_FLOAT_LIT\nFactor: MP_STRING_LIT\nFactor: MP_TRUE\nFactor: MP_FALSE\nFactor: MP_NOT Factor\nFactor: MP_LPAREN Expression MP_RPAREN\nFactor: FunctionIdentifier OptionalActualParameterList\nProgramIdentifier: MP_IDENTIFIER\nVariableIdentifier: MP_IDENTIFIER\nProcedureIdentifier: MP_IDENTIFIER\nFunctionIdentifier: MP_IDENTIFIER\nBooleanExpression: Expression\nOrdinalExpression: Expression\nIdentifierList: MP_IDENTIFIER IdentifierTail\nIdentifierTail: MP_COMMA MP_IDENTIFIER IdentifierTail\nIdentifierTail: lambda\nFactor: VariableIdentifier'.split('\n');
 
+var preceding = {};
+
 if (!Object.assign) {
   Object.defineProperty(Object, 'assign', {
     enumerable: false,
@@ -72,7 +74,7 @@ var makeTableRecurse = function (term, dict, grammar) {
         
         matches.forEach(function (a) {
             a.terminals.forEach(function (b) {
-                dict[start][b] = dict[start][b] ? union_arrays(dict[start][b], [a.index]) : [a.index];
+                dict[start][b] = dict[start][b] ? union_arrays(dict[start][b], [term.index]) : [term.index];
             });
         });
 
@@ -85,6 +87,53 @@ var makeTableRecurse = function (term, dict, grammar) {
     return { index: term.index, terminals: terms };
 };
 
+var generatePreceeding = function (term, dict) {
+    var start = term.rule.split(':')[0];
+    var end = term.rule.split(':')[1].split(' ').slice(1);
+    
+    for (var i = end.length; i !== 0; i--) {
+        if (dict[end[i-1]] && dict[end[i-1]]['lambda']) {
+            preceding[end[i]] = preceding[end[i]] ? union_arrays(preceding[end[i]], [end[i-1]]) : [end[i-1]];
+        }
+    }
+}
+
+var applyEpsilon = function (term, application, dict, grammar) {
+    var lambdaPointer = dict[term]['lambda'];
+    if (dict[application]) {
+        for (var key in dict[application]) {
+            dict[term][key] = dict[term][key] ? union_arrays(dict[term][key], lambdaPointer) : lambdaPointer;
+        }
+    } else {
+        dict[term][application] = dict[term][application] ? union_arrays(dict[term][application], lambdaPointer) : lambdaPointer;
+    }
+
+    // Propagate to preceding
+    for (var key in preceding[term]) {
+        if (dict[key] !== undefined) {
+            applyEpsilon(preceding[term][key], application, dict, grammar);
+        }
+    }
+
+    // Propagate to nested
+    lambdaPointer.forEach(function (a) {
+        var rule = grammar[a];
+        end = rule.rule.split(':')[1].split(' ').slice(1);
+
+        end.forEach(function (b) { if (dict[key] !== undefined) { applyEpsilon(b, application, dict, grammar); } });
+    });
+}
+var epsilonPostProcess = function (term, dict, grammar) {
+    var start = term.rule.split(':')[0];
+    var end = term.rule.split(':')[1].split(' ').slice(1);
+    
+    for (var i = end.length; i !== 0; i--) {
+        if (dict[end[i-1]] && dict[end[i-1]]['lambda']) {
+            applyEpsilon(end[i-1], end[i], dict, grammar);
+        }
+    }
+}
+
 var makeTable = function (arr) {
 	var grammar = arr.map(function (a,b) { return { rule: a, index: b } });
 	var lookup = grammar.reduce(function (a,b) { a[b.rule.split(':')[0]] = {}; return a; }, {});
@@ -92,10 +141,13 @@ var makeTable = function (arr) {
     grammar.forEach(function (a) {
         makeTableRecurse(a, lookup, grammar);
     });
+    grammar.forEach(function (a) { generatePreceeding(a, lookup); });
+    grammar.forEach(function (a) { epsilonPostProcess(a, lookup, grammar); });
 
     return lookup;
 };
 
+debugger;
 var table = makeTable(arr);
 var tokens = Object.keys(Object.keys(table).reduce(function (a, b) {
     return Object.assign(a, table[b]);
