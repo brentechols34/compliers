bcbce4ab0711a9085cc35f797d0e685d0fbaf7d1
Kalvyn-Lu <kalvynlu@gmail.com>
2015-03-24 12:42:47 -0600

    Changed Symbol Table names to something more reasonable

--- parser/SymbolTable.java
+++ parser/SymbolTable.java
@@ -24,19 +24,24 @@ public class SymbolTable {
 	}
 	
 	//Variables
-	public void addEntry(String lexeme, String type, String kind, int size){
+	public void addVariable(String lexeme, String type, String kind, int size){
 		TableEntry entry = new TableEntry(type,kind,size);
 		entries.put(lexeme, entry);
 	}
 
 	//Parameters
-	public void addEntry(String lexeme, String type, String kind, String mode, int size){
+	public void addParameter(String lexeme, String type, String kind, String mode, int size){
 		TableEntry entry = new TableEntry(type,kind,mode,size);
 		entries.put(lexeme, entry);
 	}
 	
 	//Procedures/functions
-	public void addEntry(String lexeme, String kind, String[] parameters){
+	public void addProcedure(String lexeme, String kind, String[] parameters){
+		TableEntry entry = new TableEntry(kind,parameters);
+		entries.put(lexeme, entry);
+	}
+	
+	public void addFunction(String lexeme, String kind, String[] parameters){
 		TableEntry entry = new TableEntry(kind,parameters);
 		entries.put(lexeme, entry);
 	}
