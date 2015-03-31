462e5aba061cdae3af5b97c85a1075de8e5cacbb
Kalvyn-Lu <kalvynlu@gmail.com>
2015-03-31 12:58:23 -0600

    Symbol Table nesting level Stack handling done, now to actually populate it

--- parser/Parse.java
+++ parser/Parse.java
@@ -238,33 +238,4 @@ public class Parse {
 		ArrayList<String> arrl = new ArrayList<>(Arrays.asList(splt));
 		return arrl;
 	}
-
-	//PopulateSymbolTable
-	//I don't know if you can do this generically. functions and procedures require different rules.
-	public SymbolTable popSymTab(Token[] tokens){
-		//Loop tokens array. Find "program","procedure","function". Create new Symbol table
-		//Set variable "name"(next token)
-		//Separate loop to loop through rest of the tokens
-			//Populate table with Variables/procedures/functions
-			//if "procedure" or "function",remember its name (next token)
-			//and ignore all until you see end --(procName)
-			//continue adding to symbol table
-		Token name;
-                int labelCount = 0;
-		for (int i = 0; i < tokens.length; i++){
-                    
-                    if(tokens[i].val.equals("program") ||
-                            tokens[i].val.equals("procedure") ||
-                            tokens[i].val.equals("function")){
-                        
-                        name = tokens[i + 1];
-                        //Missing NestingLevel Code
-                        SymbolTable st = new SymbolTable(name.val,("L" + labelCount),0);
-                        for (int j = i + 1; j < tokens.length; j++){
-                            //populate table
-                        }
-                    }
-		}
-		return null;
-	}
 }
