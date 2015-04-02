a546135253191204beee1dcaebae1fce35034478
Kalvyn-Lu <kalvynlu@gmail.com>
2015-04-02 13:12:51 -0600

    Crappy symbol table working?? Haven't tested

--- parser/TableControl.java
+++ parser/TableControl.java
@@ -12,10 +12,13 @@ public class TableControl {
 		this.tokens = tokens;
 	}
 	
+	
+	//Token Stream is array
 	public ArrayList<SymbolTable> popSymTab(){
 		
 		ArrayList<SymbolTable> SymbolTables = new ArrayList<>();	//What we return
 		Stack<SymbolTable> tableStack = new Stack<>();
+		String name;
 		int nestingLevel = 0;
 		int labelCount = 0;
 		int offset = 0;
@@ -35,7 +38,7 @@ public class TableControl {
 			}
 			
 			//Saves last token that is not an integer
-			if(tokens[i].type == TokenType.MP_IDENTIFIER){
+			if(tokens[i].type != TokenType.MP_IDENTIFIER || !tokens[i].val.equals("in") ||!tokens[i].val.equals("out")){
 				lastNotId = tokens[i];
 			}
 			
@@ -56,10 +59,12 @@ public class TableControl {
 			//If token is an identifier, add new TableEntry to the SymbolTable on top of the stack. 
 			if(tokens[i].type == TokenType.MP_IDENTIFIER){
 				if(lastNotId.type == TokenType.MP_PROCEDURE || lastNotId.type == TokenType.MP_FUNCTION){
-					tableStack.peek().addProcFunc(tokens[i].val,"Procedure", new ArrayList<String>()); //Parameters??
+					tableStack.peek().addProcFunc(tokens[i].val,"Procedure", new ArrayList<String>());
+					name = tokens[i].val;
 				}else if(isParam){                                                                                    
 					//If it is a parameter
-					tableStack.peek().addParameter(tokens[i].val,lastNotId.type, "parameter", tokens[i - 1].val, tableStack.peek().size());//parameters?
+					tableStack.peek().addParameter(tokens[i].val,lastNotId.type, "parameter", tokens[i - 1].val, tableStack.peek().size()-1);
+					tableStack.peek().entries.get(tokens[i].val).parameters.add(tokens[i].val);
 				}else {
 					//If it is a variable
 					tableStack.peek().addVariable(tokens[i].val, lastNotId.type, "variable", tableStack.peek().size());
