747f78b2d2f90ed3bfaf6e281e41a5d296e49357
NinjaKL <kalvynlu@gmail.com>
2015-04-20 23:19:14 -0600

    .

--- parser/SymbolTableController.java
+++ parser/SymbolTableController.java
@@ -4,19 +4,22 @@ import util.Token;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * Created by brentechols on 4/8/15.
  */
 public class SymbolTableController {
     ArrayList<Token> tokens;
+    Stack<SymbolTable> symbolStack;
 
     public SymbolTableController(ArrayList<Token> tokens) throws IOException {
         this.tokens = tokens;
+        symbolStack = new Stack<>();
     }
 
     public void Apply(RuleApplication application) {
-
+        
     }
 
     public void Undo(RuleApplication application) {
