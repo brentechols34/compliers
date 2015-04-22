8636e2bcc8ab3de6cb3f78f45e56a2538ac82c79
NinjaKL <kalvynlu@gmail.com>
2015-04-21 20:01:37 -0600

    .

--- SemanticAnalyzer/SemanticAnalyzer.java
+++ SemanticAnalyzer/SemanticAnalyzer.java
@@ -6,21 +6,29 @@ import java.io.PrintWriter;
 import java.util.Stack;
 import java.util.ArrayList;
 import parser.RuleApplication;
+import util.*;
+
 
 public class SemanticAnalyzer {
 	
 	PrintWriter pw;
 	Stack<CodeChunk> code;
 	ArrayList<RuleApplication> rules;
-	
+        	
 	public SemanticAnalyzer(String fname,ArrayList<RuleApplication> rules) throws FileNotFoundException{
 		pw = new PrintWriter(new File(fname));
 		code = new Stack<>();
 		this.rules = rules;
 	}
-	
-	public void convert(File semAnalyze){
-		
+        
+	public CodeChunk convert(ArrayList<Token> tokens){
+            Token curToken = tokens.get(0);
+            switch(curToken.type){
+                case MP_INTEGER:
+                    CodeChunk cc = new CodeChunk();
+                    
+            }
+            return null;
 	}	
 	
 	public void printToFile(){
