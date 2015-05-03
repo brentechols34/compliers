ceb57b119ea597a9ec07cf640a26357365945aac
NinjaKL <kalvynlu@gmail.com>
2015-05-03 16:49:56 -0600

    .

---
+++ parser/LabelProvider.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+/**
+ *
+ * @author NinjaKL
+ */
+public class LabelProvider {
+    int labelNumber;
+    
+    public LabelProvider(){
+        labelNumber = 0;
+    }
+    
+    public String nextLabel(){
+        return "L" + labelNumber++ + ":";
+    }
+     
+    public String peekLabel(int offset){
+        return "L" + (labelNumber + offset) + ":";
+    }
+}
